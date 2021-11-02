b0a6def242ab870530b341af2f84f7e3b4786eed
not1214 <91748221+not1214@users.noreply.github.com>
2021-11-02 19:53:09 +0900

    Update favorite

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resources :favorites, only: [:create, :destroy]
+  end
   
   
   get 'home/about' => 'homes#about'
