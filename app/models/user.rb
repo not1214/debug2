31c8cb64e8ee9d0ae378e85ac89173c07d5d0a1b
not1214 <91748221+not1214@users.noreply.github.com>
2021-11-04 00:05:56 +0900

    Update follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+  has_many :reverse_of_ralationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+
+  def followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).exists?
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+  
   resources :books do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
